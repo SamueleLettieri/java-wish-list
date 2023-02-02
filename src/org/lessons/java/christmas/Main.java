f077b68af8cba3604d30ef3d21aef48f830ff189
SamueleLettieri <samuele2073@gmail.com>
2023-02-02 15:10:03 +0100

    fine esercizio

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,35 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();		
+		
+		String wish, exit = "";
+		
+		Scanner s = new Scanner(System.in);
+		
+		
+		while (!exit.equals("fine")) {
+			System.out.print("Inserisci un desiderio: ");
+			wish = s.nextLine();
+			
+			list.add(wish);
+			
+			System.out.println("La lista contiene: " + list.size() + " desideri" );
+			System.out.print("Se vuoi fermarti digita fine, senno digita continua: ");
+			exit = s.nextLine();
+			
+		}
+		
+		s.close();
+		
+		list.sort(null);
+		System.out.println("Lista desideri: " + list);
+	}
+	
+}
